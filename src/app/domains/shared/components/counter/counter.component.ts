d6bd9ff939c4c710cca2aa694214ec5c1353edaf
José Darío González Cruz <darioucv@ucvvirtual.edu.pe>
2024-03-18 12:15:21 -0500

    life cycle components

---
+++ src/app/domains/shared/components/counter/counter.component.ts
@@ -0,0 +1,24 @@
+import { Component, Input, SimpleChange } from '@angular/core';
+
+@Component({
+  selector: 'app-counter',
+  standalone: true,
+  imports: [],
+  templateUrl: './counter.component.html',
+  styleUrl: './counter.component.css'
+})
+export class CounterComponent {
+  @Input({required:true}) duration = 0;
+  @Input({required:true}) message = '';
+
+  constructor(){
+    console.log('constructor');
+    console.log('-'.repeat(10));
+  }
+
+  ngOnChanges(changes:SimpleChange){
+    console.log('ngOnChanges');
+    console.log('-'.repeat(10));
+    console.log(changes);
+  }
+}
